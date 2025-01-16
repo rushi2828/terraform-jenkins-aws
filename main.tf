cf82f6e8ea72b3e065fd0e709ae4298cd121de90
Rushikesh Mane <33669698+rushi2828@users.noreply.github.com>
2025-01-16 16:00:51 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = var.aws_region # Change the region as per your requirement
+}
+
+resource "aws_instance" "jenkins" {
+  ami                    = var.ami # Amazon Linux 2 AMI
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
+  subnet_id              = aws_subnet.main_subnet.id
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install java-11-openjdk -y
+              curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+              sudo yum install jenkins -y
+              sudo systemctl enable jenkins
+              sudo systemctl start jenkins
+              EOF
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Main-VPC"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Main-Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Main-Internet-Gateway"
+  }
+}
+
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "Main-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "main_route_table_assoc" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
+}
+
+
+resource "aws_security_group" "jenkins-sg" {
+  name_prefix = "jenkins-sg"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow Jenkins access from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region = "ap-south-1"
+ami        = "ami-07b69f62c1d38b012"
